1d590fb6f24db4cf5343dc880cddce02326f25d6
virchaca <you@example.com>
2023-10-20 15:55:46 +0200

    adding starter kit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+// import viteLogo from '/vite.svg'
+import '../styles/App.scss'
+// import ls from './services/localStorage';
+
+function App() {
+  
+  return (
+    <></>      
+  )
+}
+
+export default App
